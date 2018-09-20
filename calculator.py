13ff3df067a83bd3095ceabfa2243fac7d922b04
Andrew Park <apark46.work@gmail.com>
2018-09-20 00:07:53 -0700

    Calculator Finished implementing TODOs Yeah

--- calculator.py
+++ calculator.py
@@ -45,13 +45,54 @@ To submit your homework:
 def add(*args):
     """ Returns a STRING with the sum of the arguments """
 
-    # TODO: Fill sum with the correct value, based on the
-    # args provided.
-    sum = "0"
+    additives = sum(args)
 
-    return sum
+    return str(additives)
 
-# TODO: Add functions for handling more arithmetic operations.
+def sub(*args):
+    """ Returns a STRING with the difference of the arguments """
+    diff = args[0]
+    for x in args[1:]:
+        diff -= x
+
+    return str(diff)
+
+def mult(*args):
+    """ Returns a STRING with the product of the arguments """
+
+    product = args[0]
+    for x in args[1:]:
+        product = product*x
+
+    return str(product)
+
+def div(*args):
+    """ Returns a STRING with the quotient of the arguments """
+
+    quotient = args[0]
+    for x in args[1:]:
+        quotient = quotient/x
+
+    return str(quotient)
+
+
+def home():
+    """ Returns a STRING for the calculator's home page"""
+    ex_urls = [("/multiply/3/5", '15'),
+               ("/add/23/42", '65'),
+               ("/subtract/23/42", '-19'),
+               ("/divide/22/11", '2'),               
+    ]
+    body = "<h1>Single Operation Calculator</h1>\n"
+    body += "<h3>How to use this page: </h3>\n"
+    body += "http://localhost:8080/FUNCTION/NUM1/NUM2/...\n"
+    body += "<h3>Examples</h3>\n"
+    body += "<ul>\n"
+    for url, answ in ex_urls:
+        body += "<li><a href="+url+'>'
+        body += "http://localhost:8080"+url+'</a> => '+answ+'</li>\n'
+    body += "</ul>"
+    return body
 
 def resolve_path(path):
     """
@@ -59,26 +100,48 @@ def resolve_path(path):
     arguments.
     """
 
-    # TODO: Provide correct values for func and args. The
-    # examples provide the correct *syntax*, but you should
-    # determine the actual values of func and args using the
-    # path.
-    func = add
-    args = ['25', '32']
+    functions = {
+        '': home,
+        'add': add,
+        'subtract': sub,
+        'multiply': mult,
+        'divide': div,
+    }
+    
+    path = path.strip('/').split('/')
+
+    try:
+        func = functions[path[0]]
+        args = [float(x) for x in path[1:]]
+    except (KeyError, ValueError):
+        raise NameError
 
     return func, args
 
 def application(environ, start_response):
-    # TODO: Your application code from the book database
-    # work here as well! Remember that your application must
-    # invoke start_response(status, headers) and also return
-    # the body of the response in BYTE encoding.
-    #
-    # TODO (bonus): Add error handling for a user attempting
-    # to divide by zero.
-    pass
+
+    headers = [("Content-type", "text/html")]
+    body = "<a href=/>Home</a>"
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body += "<h1>" + func(*args) + "</h1>"
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body += "<h1>Invalid Input</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body += "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
 
 if __name__ == '__main__':
-    # TODO: Insert the same boilerplate wsgiref simple
-    # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
